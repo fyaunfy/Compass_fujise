df87a48cd1035970a3fa7ded99da909e2f9a7ea0
fujiseyuya <fymac0625@icloud.com>
2022-07-27 14:38:57 +0900

    初期設定です。

--- AtlasManagementSystem-master/database/seeds/DatabaseSeeder.php
+++ AtlasManagementSystem-master/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        // $this->call(SubjectsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
+
 }
--- AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
+++ AtlasManagementSystem-master/database/seeds/UsersTableSeeder.php
@@ -12,6 +12,15 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            ['over_name' => '藤瀬'],
+            ['under_name' => '悠也'],
+            ['over_name_kana' => 'フジセ'],
+            ['under_name_kana' => 'ユウヤ'],
+            ['mail_address' => 'hizen0625@gmail.com'],
+            ['sex' => '男'],
+            ['birth_day' => '6月25日'],
+            ['password' => bcrypt('password')]
+        ]);
     }
 }
